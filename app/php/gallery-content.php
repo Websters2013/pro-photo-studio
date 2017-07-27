ce43c760d96ebf84ebf382682e6efcf47eb9cfec
iversontb <petruninevgeniy@gmail.com>
2017-07-27 17:19:44 +0300

    index page

--- app/php/gallery-content.php
+++ app/php/gallery-content.php
@@ -4,60 +4,44 @@ $loadedGroup = $_GET['loadedGroup'];
 
 if ( $loadedGroup == 0 ){
 
-    //  "has_items" - the number of not downloaded images
-    //  "title" - text which shows at hover on element
-    //  "dummy" - path to preview picture
-    //  "href" - path to main picture, which shows in popup, or a link to the video
-    //  "video" - if it is a block with a link to video frame
-
     $json_data = '{
                     "has_items": 1,
                     "items":[
-
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-1.jpg",
-                            "href": "pic/gallery__pic-1.jpg"
+                            "type": "all on-white",
+                            "dummy": "pic/gallery__pic-1.jpg"
                         },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-2.jpg",
-                            "href": "pic/gallery__pic-2.jpg"
+                            "type": "all apparel",
+                            "dummy": "pic/gallery__pic-2.jpg"
                         },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-3.jpg",
-                            "href": "pic/gallery__pic-3.jpg"
+                            "type": "all jewelry",
+                            "dummy": "pic/gallery__pic-3.jpg"
                         },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-4.jpg",
-                            "href": "pic/gallery__pic-4.jpg"
+                            "type": "all on-white video",
+                            "dummy": "pic/gallery__pic-6.jpg"
                         },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-5.jpg",
-                            "video": "https://www.youtube.com/embed/3PYLnSrP3eE"
+                            "type": "all custom-shots",
+                            "dummy": "pic/gallery__pic-4.jpg"
                         },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-6.jpg",
-                            "href": "pic/gallery__pic-6.jpg"
+                            "type": "all on-white apparel",
+                            "dummy": "pic/gallery__pic-5.jpg"
                         },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-7.jpg",
-                            "href": "pic/gallery__pic-7.jpg"
+                            "type": "all jewelry",
+                            "dummy": "pic/gallery__pic-7.jpg"
                         },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-8.jpg",
-                            "href": "pic/gallery__pic-8.jpg"
+                            "type": "all on-white custom-shots",
+                            "dummy": "pic/gallery__pic-9.jpg"
                         },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-9.jpg",
-                            "href": "pic/gallery__pic-9.jpg"
+                            "type": "all apparel",
+                            "dummy": "pic/gallery__pic-8.jpg"
                         }
 
                     ]
@@ -67,104 +51,44 @@ if ( $loadedGroup == 0 ){
 } else if ( $loadedGroup == 1 ){
 
     $json_data = '{
-                    "has_items": 1,
-                    "items":[
-
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-1.jpg",
-                            "href": "pic/gallery__pic-1.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-3.jpg",
-                            "href": "pic/gallery__pic-3.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-5.jpg",
-                            "href": "pic/gallery__pic-5.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-6.jpg",
-                            "href": "pic/gallery__pic-6.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-4.jpg",
-                            "video": "https://www.youtube.com/embed/3PYLnSrP3eE"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-7.jpg",
-                            "href": "pic/gallery__pic-7.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-2.jpg",
-                            "href": "pic/gallery__pic-2.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-8.jpg",
-                            "href": "pic/gallery__pic-8.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-9.jpg",
-                            "href": "pic/gallery__pic-9.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-10.jpg",
-                            "href": "pic/gallery__pic-10.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-11.jpg",
-                            "href": "pic/gallery__pic11.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-12.jpg",
-                            "href": "pic/gallery__pic-12.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-13.jpg",
-                            "href": "pic/gallery__pic-13.jpg"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-4.jpg",
-                            "video": "https://www.youtube.com/embed/3PYLnSrP3eE"
-                        },
-                        {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-15.jpg",
-                            "href": "pic/gallery__pic-15.jpg"
-                        }
-
-                    ]
-                }';
-
-} else {
-
-    $json_data = '{
                     "has_items": 0,
                     "items":[
-
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-1.jpg",
-                            "href": "pic/gallery__pic-1.jpg"
-                            },
+                            "type": "all on-white",
+                            "dummy": "pic/gallery__pic-1.jpg"
+                        },
                         {
-                            "title": "VIEW",
-                            "dummy": "pic/gallery__pic-3.jpg",
-                            "video": "https://www.youtube.com/embed/YykjpeuMNEk"
-                            }
+                            "type": "all apparel",
+                            "dummy": "pic/gallery__pic-2.jpg"
+                        },
+                        {
+                            "type": "all jewelry",
+                            "dummy": "pic/gallery__pic-3.jpg"
+                        },
+                        {
+                            "type": "all on-white video",
+                            "dummy": "pic/gallery__pic-6.jpg"
+                        },
+                        {
+                            "type": "all custom-shots",
+                            "dummy": "pic/gallery__pic-4.jpg"
+                        },
+                        {
+                            "type": "all on-white apparel",
+                            "dummy": "pic/gallery__pic-5.jpg"
+                        },
+                        {
+                            "type": "all jewelry",
+                            "dummy": "pic/gallery__pic-7.jpg"
+                        },
+                        {
+                            "type": "all on-white custom-shots",
+                            "dummy": "pic/gallery__pic-9.jpg"
+                        },
+                        {
+                            "type": "all apparel",
+                            "dummy": "pic/gallery__pic-8.jpg"
+                        }
 
                     ]
                 }';
