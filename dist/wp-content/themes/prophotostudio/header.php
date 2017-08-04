c58a90cc8d2d797570b38e7e24fdcb5cd9eddf67
vetal <akumuliation@gmail.com>
2017-08-04 11:01:59 +0300

    Merge

--- dist/wp-content/themes/prophotostudio/header.php
+++ dist/wp-content/themes/prophotostudio/header.php
@@ -71,26 +71,6 @@ if( $locations && isset($locations[ $menu_name ]) ){
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="format-detection" content="telephone=no">
     <meta name="format-detection" content="address=no">
-    <link rel="shortcut icon" href="<?= get_template_directory_uri().'/assets/img/favicon.ico'; ?>">
-    <link rel="icon" sizes="16x16 32x32 64x64" href="<?= get_template_directory_uri().'/assets/img/favicon.ico'; ?>">
-    <link rel="icon" type="image/png" sizes="196x196" href="<?= get_template_directory_uri().'/assets/img/favicon-192.png'; ?>">
-    <link rel="icon" type="image/png" sizes="160x160" href="<?= get_template_directory_uri().'/assets/img/favicon-160.png'; ?>">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= get_template_directory_uri().'/assets/img/favicon-96.png'; ?>">
-    <link rel="icon" type="image/png" sizes="64x64" href="<?= get_template_directory_uri().'/assets/img/favicon-64.png'; ?>">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= get_template_directory_uri().'/assets/img/favicon-32.png'; ?>">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= get_template_directory_uri().'/assets/img/favicon-16.png'; ?>">
-    <link rel="apple-touch-icon" href="<?= get_template_directory_uri().'/assets/img/favicon-57.png'; ?>">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= get_template_directory_uri().'/assets/img/favicon-114.png'; ?>">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= get_template_directory_uri().'/assets/img/favicon-72.png'; ?>">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= get_template_directory_uri().'/assets/img/favicon-144.png'; ?>">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= get_template_directory_uri().'/assets/img/favicon-60.png'; ?>">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= get_template_directory_uri().'/assets/img/favicon-120.png'; ?>">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= get_template_directory_uri().'/assets/img/favicon-76.png'; ?>">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= get_template_directory_uri().'/assets/img/favicon-152.png'; ?>">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= get_template_directory_uri().'/assets/img/favicon-180.png'; ?>">
-    <meta name="msapplication-TileColor" content="#FFFFFF">
-    <meta name="msapplication-TileImage" content="<?= get_template_directory_uri().'/assets/img/favicon-144.png'; ?>">
-    <meta name="msapplication-config" content="<?= get_template_directory_uri().'/assets/img/browserconfig.xml'; ?>">
     <title>Title</title>
 
 <?php wp_head(); ?>
